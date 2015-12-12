4c64851b96a67ed07abbd190ca696585a8846b47
Maggie Love <maggie.love1991@gmail.com>
2015-12-12 17:36:14 -0500

    set up app.js entry point file

---
+++ api/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+let express = require('express');
+let path = require('path');
+let cors = require('cors');
+let logger = require('morgan');
+let request = require('request');
+let bodyParser = require('body-parser');
+let jwt = require('jsonwebtoken');
+let app = express();
+
+let mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/graffitiApp');
+
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error: '));
+db.once('open', (callback) => {
+  console.log('Mongoose connected.');
+});
+
+let routes = require('./config/routes');
+app.use(cors());
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use('/', express.static(__dirname + '/public'));
+app.use('/scripts', express.static(__dirname + '/node_modules'));
+app.use('/', routes)
+
+let server = app.listen(process.env.PORT || 3000, () => {
+  let host = server.address().address;
+  let port = server.address().port;
+  console.log('express running', host, port);
+})
