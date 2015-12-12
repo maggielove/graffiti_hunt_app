08a7c7b8341a4bd85db1440d40fe9ecc585b39f5
Maggie Love <maggie.love1991@gmail.com>
2015-12-12 18:19:06 -0500

    added basic required vars to routes.js

---
+++ api/config/routes.js
@@ -0,0 +1,17 @@
+'use strict';
+let express = require('express');
+let router = express.Router();
+let bodyParser = require('body-parser');
+let methodOverride = require('method-override');
+
+let usersController = require('../controllers/users');
+let placesController = require('../controllers/places');
+
+
+
+
+
+
+
+
+module.exports = router;
